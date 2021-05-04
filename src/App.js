3d3aa18401998e32ab0b84789c3eef38fd36a32e
Lucy Donnelly <lucy.donnelly@autotrader.co.uk>
2021-05-04 11:17:44 +0100

    feat:render questions to screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+ 
+  const questions  = [{
+
+    question: "According to Schon, how many types of reflection are there?",
+    answer: "2"
+    },
+    {
+      question: "What is the collective name for a group of giraffe’s?",
+      answer: "Tower"
+    }, 
+    {
+      question: "What movie is this quotation from? 'People will think you're up to something'",
+      answer: "Harry Potter and the Philosopher's Stone"
+    },
+    {
+      question: "What is the command to use when you want to list all the files in a directory?",
+      answer: "ls"
+    }
+  ]
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       { questions.map( question => <p>{ question.question }</p>) }
     </div>
   );
 }
