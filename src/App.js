63610274ce52edcba119af77f11c35aeca10cf09
Lucy Donnelly <lucy.donnelly@autotrader.co.uk>
2021-05-04 12:22:01 +0100

    feat:cards added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
-
-import './App.css';
+import "./App.css";
+import Card from "./components/Card";
 
 function App() {
- 
-  const questions  = [{
-
-    question: "According to Schon, how many types of reflection are there?",
-    answer: "2"
+  const questions = [
+    {
+      question: "According to Schon, how many types of reflection are there?",
+      answer: "2",
+      id: 0
     },
     {
       question: "What is the collective name for a group of giraffe’s?",
-      answer: "Tower"
-    }, 
-    {
-      question: "What movie is this quotation from? 'People will think you're up to something'",
-      answer: "Harry Potter and the Philosopher's Stone"
+      answer: "Tower",
+      id: 1
     },
     {
-      question: "What is the command to use when you want to list all the files in a directory?",
-      answer: "ls"
-    }
-  ]
-
+      question:
+        "What movie is this quotation from? 'People will think you're up to something'",
+      answer: "Harry Potter and the Philosopher's Stone",
+      id: 2
+    },
+    {
+      question:
+        "What is the command to use when you want to list all the files in a directory?",
+      answer: "ls",
+      id: 3
+    },
+  ];
 
   return (
     <div className="App">
-       { questions.map( question => <p>{ question.question }</p>) }
+      {questions.map(({ question, answer, id}) => {
+        return <Card key={id} question={question} answer={answer} />;
+      })}
     </div>
   );
 }
